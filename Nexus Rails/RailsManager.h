abbacfdc44d3996862d8e98350abede5e1897ed9
Bill <argo_15@hotmail.com>
2012-04-21 01:18:51 -0400

    fuckoff

---
+++ Nexus Rails/RailsManager.h
@@ -0,0 +1,22 @@
+#ifndef RAILSMANAGER_H
+#define RAILSMANAGER_H
+
+#include "Matrix.h"
+#include <vector>
+using namespace std;
+
+class RailsManager {
+private:
+	int numRails;
+	vector<Vector3> *railPositions;
+	Vector3 *railColors;
+
+public:
+	RailsManager();
+	~RailsManager() {}
+
+	void reloadRails();
+	void drawRails();
+};
+
+#endif
